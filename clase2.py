effc14495ca837b1cc879fad2c780b62066ca78e
Maximiliano Torreblanca <torreblancamd@gmail.com>
2024-09-18 09:17:52 -0300

    Add files via upload

---
+++ clase2.py
@@ -0,0 +1,25 @@
+
+#VARIABLES 
+
+nombre_usuario = "Juan"
+edad_usuario = 30
+
+
+#OPERADOR DE ASIGNACION =
+
+nombre_usuario = "Pedro"
+
+edad_usuario = 25 
+
+#CASE SENSITIVE
+nombre_USUARIO = "Marcos"
+
+print( nombre_usuario )
+
+
+#VARIABLES EN PYTHON DINAMICAMENTE TIPADAS
+nombre_usuario = 44
+edad_usuario = "Rojo"
+
+print(nombre_usuario)
+print(edad_usuario)
---
+++ clase2b.py
@@ -0,0 +1,92 @@
+
+#DATA TYPE
+
+#STRING--->CADENA DE CARACTERES
+nombre_usuario = "Juan"
+
+#INT--->ENTERO
+edad_usuario = 30
+
+
+#FLOAT
+nota_usuario = 8.5
+
+
+#BOOL--->LOGICOS
+verdadero = True
+falso = False
+
+
+
+
+# OPERADORES ARITMETICOS
+
+
+#SUMA +
+
+suma = 30 + 40
+print(suma)
+
+
+suma = 70 + 10
+print(suma)
+
+
+num_uno = 25
+num_dos = 50
+
+suma = num_uno + 15
+print(suma)
+
+
+suma = num_dos + num_uno
+print(suma)#suma: 75
+
+
+suma = suma + num_uno 
+print(suma )
+
+
+
+#RESTA -
+
+resta = 100 - 40
+print( resta )
+
+
+#MULTIPLICACION *
+
+multi = 40 * 3
+print( multi )
+
+
+#DIVISION /
+
+divi=50/2
+print(divi)
+
+
+
+
+#CONCATENACION
+suma = "Juan" + "Lopez"
+print(suma)
+
+
+suma = "90" + "77"
+print(suma)
+
+#ERROR
+#suma = 40 + "Pedro"
+#print(suma)
+
+
+suma = 30 + 40 * 2
+print(suma)
+
+suma = (30+40+10+30) * 2
+
+
+#PRINT
+
+print("HOLA", "CHAU", 33)
